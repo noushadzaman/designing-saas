540b05f501c0d0c761967fca2647aa3f8d2db244
Noushad <noushadozi333@gmail.com>
2024-12-17 14:49:19 +0600

    nav & layout

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      CUSTOM_API_URL: 'https://example.com/api',
+    },
+  };
+  
+  module.exports = nextConfig;
+  
