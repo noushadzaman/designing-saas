0177e6ac95d0187199f4e8f223a38e0f588f8441
Noushad <noushadozi333@gmail.com>
2024-12-22 14:52:39 +0600

    logo & intro animation

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -2,15 +2,14 @@ import { HTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
 
 export default function Tag(props: HTMLAttributes<HTMLDivElement>) {
-    const { className, children, ...otherProps } = props;
+    const {  children, ...otherProps } = props;
 
     return (
         <div
             className={
                 (twMerge(
                     "inline-flex border border-lime-400 gap-2 text-lime-400 px-3 py-1 rounded-full uppercase items-center"
-                ),
-                className)
+                ))
             }
             {...otherProps}
         >
